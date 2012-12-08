a5ecb8b16bc943353d67462f6192997a38c09189
issyl0 <isabell@issyl0.co.uk>
2012-12-08 21:33:46 +0000

    Change the ALMOST TOO HIGH wording to RAISED.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function check_blood_pressure_values() {
 		$("<p>Your blood pressure is <span class='bp-bad'>TOO HIGH</span>.<br /><input type='button' value='Help!' class='infobox-slider' onclick='infobox_slider_control()'></input></p>")
 			.appendTo($("#content"))
 	} else if (bp_top > 120 || bp_bottom > 80) {
-		$("<p>Your blood pressure is <span class='bp-moderate'>ALMOST TOO HIGH</span>.</p>")
+		$("<p>Your blood pressure is <span class='bp-moderate'>SLIGHTLY HIGH</span>.</p>")
 			.appendTo("#content")
 	} else if (bp_top > 90 || bp_bottom > 60) {
 		$("<p>Your blood pressure is <span id='bp-good'>FINE</span>.</p>")
