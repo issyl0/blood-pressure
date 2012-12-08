b7fad2f9b295828ca2c74d28a8c38f3b66a2a2c6
issyl0 <isabell@issyl0.co.uk>
2012-12-08 20:59:47 +0000

    The calculation logic is no longer flawed.

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ function check_blood_pressure_values() {
 	var bp_top = $('#bp-top').val()
 	var bp_bottom = $('#bp-bottom').val()
 
-	if (bp_top > 120 && bp_bottom > 80) {
+	if (bp_top > 140 || bp_bottom > 90) {
 		$("<p>Your blood pressure is <span class='bp-bad'>TOO HIGH</span>.<br /><input type='button' value='Help!' class='infobox-slider' onclick='infobox_slider_control()'></input></p>")
 			.appendTo($("#content"))
-	} else if ((bp_top < 90 && bp_bottom < 40) && (bp_top > 1 && bp_bottom > 1)) {
-		$("<p>Your blood pressure is <span class='bp-bad'>TOO LOW</span>.</p>")
-			.appendTo($("#content"))
-	} else if (bp_top == 0 && bp_bottom == 0) {
-		$("<p>You must be <span class='bp-bad'>DEAD</span>.</p>").appendTo($("#content"))
-	} else {
+	} else if (bp_top > 120 || bp_bottom > 80) {
+		$("<p>Your blood pressure is <span class='bp-moderate'>ALMOST TOO HIGH</span>.</p>")
+			.appendTo("#content")
+	} else if (bp_top > 90 || bp_bottom > 60) {
 		$("<p>Your blood pressure is <span id='bp-good'>FINE</span>.</p>")
 			.appendTo($("#content"))
+	} else if (bp_top > 0 && bp_bottom > 0) {
+		$("<p>Your blood pressure is <span class='bp-bad'>TOO LOW</span>.</p>")
+			.appendTo($("#content"))
+	} else {
+		$("<p>You must be <span class='bp-bad'>DEAD</span>.</p>")
+			.appendTo("#content")
 	}
 }
