945934359258553141921ab625459c83c52ce12d
issyl0 <isabell@issyl0.co.uk>
2013-01-02 19:34:37 +0000

    Script to convert values from db into CSV format for the graph.

---
+++ bp_db_to_csv.rb
@@ -0,0 +1,25 @@
+# Convert the blood pressures from the database into a CSV file in
+# order to display the graph.
+
+require 'rubygems'
+require 'mysql'
+
+connection_info = File.open("/home/isabell/bp.txt", "r")
+connection_string = connection_info.read.chomp
+connection_info.close
+server, dbname, dbuser, dbpass = connection_string.split(':',4)
+$db_connection = Mysql.new server, dbuser, dbpass, dbname
+
+# User ID of whoever is logged in. Hardcoded until authentication is
+# implemented.
+userid = 12345
+
+# Get the data.
+bp_data = Array.new()
+bp_data = $db_connection.query "SELECT reading_time,systolic,diastolic FROM blood_pressures WHERE user_id=#{userid} ORDER BY reading_time"
+
+# Convert array to CSV.
+bp_data.each do |i|
+  date = i[0].split(" ") # We don't care about the specific time.
+  puts date[0] + "," + i[1] + "," + i[2]
+end
